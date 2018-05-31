1279533e92256e7c7730e8ff3e3fa7027eaa374b
Vukasin Jovanovic <jovanovicvukasin99@gmail.com>
2018-05-31 17:56:11 +0200

    equals i hashCode u restoran

--- src/main/java/ftn/vu/model/Restoran.java
+++ src/main/java/ftn/vu/model/Restoran.java
@@ -30,5 +30,43 @@ public class Restoran extends Identifikator{
 		this.adresa = adresa;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((adresa == null) ? 0 : adresa.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((kategorija == null) ? 0 : kategorija.hashCode());
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Restoran other = (Restoran) obj;
+		if (adresa == null) {
+			if (other.adresa != null)
+				return false;
+		} else if (!adresa.equals(other.adresa))
+			return false;
+		if (id != other.id)
+			return false;
+		if (kategorija != other.kategorija)
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		return true;
+	}
+	
+	
 
 }
